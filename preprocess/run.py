c85671864d820b3761b6aea6016a912c5a621815
Eduardo Aviles <eduardo.aviles@loanpro.io>
2023-04-20 23:59:36 -0600

    implementing all stemps

---
+++ preprocess/run.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import pandas as pd
+import wandb
+import os
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(project="exercise_5", job_type="process_data")
+    
+    logger.info("Downloading artifact")
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+    
+    df = pd.read_parquet(artifact_path)
+    
+    logger.info("Dropping duplicates")
+    df = df.drop_duplicates().reset_index(drop=True)
+    
+    logger.info("Fixing missing values")
+    
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+    
+    
+    df.to_csv(args.artifact_name)
+    
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description,
+    )
+    
+    artifact.add_file(args.artifact_name)
+    
+    logger.info("logging artifact")
+    run.log_artifact(artifact)
+    
+    os.remove(args.artifact_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Preprocess a dataset",
+        fromfile_prefix_chars="@",
+    )
+
+    parser.add_argument(
+        "--input_artifact",
+        type=str,
+        help="Fully-qualified name for the input artifact",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--artifact_name", type=str, help="Name for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_type", type=str, help="Type for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_description",
+        type=str,
+        help="Description for the artifact",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    go(args)
